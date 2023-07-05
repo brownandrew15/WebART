3972bec4ed54526f31b14dc379af97f43bfe4b55
brownandrew15 <54123841+brownandrew15@users.noreply.github.com>
2023-07-06 00:10:11 +0100

    Set up the code element behind the textarea for the ART editor
    Set up the code element behind the textarea and set the CSS of the textarea to not show apart from the cursor - this is so the code element can have syntax highlighting applied and the textarea will provide the original text to the backend

---
+++ src/main/resources/scripts/editor/editor-input.js
@@ -0,0 +1,16 @@
+
+
+class EditorInput {
+
+    static update(content) {
+
+        var hightlighted = document.getElementById("art-editor-highlighted");
+    
+        // add the highlighting here
+    
+        hightlighted.innerText = content;
+    
+    }
+
+}
+
